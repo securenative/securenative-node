a75850a915db997e2fb872807f00fae43651f802
alexivsn <alex@securenative.com>
2019-12-28 13:22:38 +0200

    Added name to error

--- src/events/error-event.ts
+++ src/events/error-event.ts
@@ -2,12 +2,14 @@ import IEvent from './event';
 import { EventTypes } from '..';
 
 export default class ErrorEvent implements IEvent {
+  private name: string;
   private message: string;
   private stackTrace: string;
   eventType: string = EventTypes.ERROR;
   ts: number;
 
   constructor(err: Error) {
+    this.name = err.name;
     this.message = err.message;
     this.stackTrace = err.stack;
     this.ts = Date.now();
