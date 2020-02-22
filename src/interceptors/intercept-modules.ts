0ab2cbdfe4d945b9dc9d8ec81ff5840c9e8c65c4
alexivsn <alex@securenative.com>
2020-02-22 18:00:33 +0200

    Added Hapi

--- src/interceptors/intercept-modules.ts
+++ src/interceptors/intercept-modules.ts
@@ -1,6 +1,7 @@
 enum InterceptModules {
   Express = "/node_modules/express/lib/express.js",
   Koa = "/node_modules/koa/lib/application.js",
+  Hapi = "/node_modules/@hapi/hapi/lib/index.js"
 };
 
 export default InterceptModules;
