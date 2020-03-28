c54bec27734f2996fdd0d6d8080ad47e63f34592
alexivsn <alex@securenative.com>
2020-03-28 17:08:04 +0300

    Action list large list effcient blocking

--- src/action-set.spec.ts
+++ src/action-set.spec.ts
@@ -7,26 +7,32 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 describe('SecureNative', () => {
+  beforeEach(() => {
+    this.actionSet = new ActionSet("ActionTest");
+  })
+
+  afterEach(() => {
+    this.actionSet.clear();
+  });
+
   it('Should create valid empty ActionSet', () => {
-    const actionSet = new ActionSet("ActionTest");
-    expect(actionSet).to.have.property("add");
-    expect(actionSet).to.have.property("has");
-    expect(actionSet).to.have.property("delete");
+    expect(this.actionSet).to.have.property("add");
+    expect(this.actionSet).to.have.property("has");
+    expect(this.actionSet).to.have.property("delete");
   });
 
   it('Should not block invalid ip', () => {
     const ip = "da0352b6df497acd526a44ca0424f4b3";
-    const actionSet = new ActionSet("ActionTest");
-    actionSet.add(SetType.IP, ip);
+    this.actionSet.add(SetType.IP, ip);
 
-    expect(actionSet.has(SetType.IP, ip)).to.be.false;
+    expect(this.actionSet.has(SetType.IP, ip)).to.be.false;
   });
 
   it('Should not block invalid expired ip', () => {
     const ip = "da0352b6df497acd526a44ca0424f4b3";
-    const actionSet = new ActionSet("ActionTest");
-    actionSet.add(SetType.IP, ip, 1583422845, 600);
-    expect(actionSet.has(SetType.IP, ip)).to.be.false;
+
+    this.actionSet.add(SetType.IP, ip, 1583422845, 600);
+    expect(this.actionSet.has(SetType.IP, ip)).to.be.false;
   });
 
   it('Should block ip, user, country forever', () => {
@@ -34,95 +40,79 @@ describe('SecureNative', () => {
     const user = "DC48C86C04DF0005FB4DE3629AB1F";
     const country = "US";
 
-    const actionSet = new ActionSet("ActionTest");
-    actionSet.add(SetType.IP, ip);
-    actionSet.add(SetType.USER, user);
-    actionSet.add(SetType.COUNTRY, country);
 
-    expect(actionSet.has(SetType.IP, ip)).to.be.true;
-    expect(actionSet.has(SetType.USER, user)).to.be.true;
-    expect(actionSet.has(SetType.COUNTRY, country)).to.be.true;
+    this.actionSet.add(SetType.IP, ip);
+    this.actionSet.add(SetType.USER, user);
+    this.actionSet.add(SetType.COUNTRY, country);
+
+    expect(this.actionSet.has(SetType.IP, ip)).to.be.true;
+    expect(this.actionSet.has(SetType.USER, user)).to.be.true;
+    expect(this.actionSet.has(SetType.COUNTRY, country)).to.be.true;
   });
 
-  it('Should block ip, user, country for 2 sec', function (done) {
+  it('Should block ip, user, country for 2 sec', (done) => {
     const ip = "10.0.0.1";
     const user = "DC48C86C04DF0005FB4DE3629AB1F";
     const country = "US";
     const timeout = 2000;
 
-    // set test timeout
-    this.timeout(timeout + 100);
+    this.actionSet.add(SetType.IP, ip, Date.now(), timeout / 1000);
+    this.actionSet.add(SetType.USER, user, Date.now(), timeout / 1000);
+    this.actionSet.add(SetType.COUNTRY, country, Date.now(), timeout / 1000);
 
-    const actionSet = new ActionSet("ActionTest");
-    actionSet.add(SetType.IP, ip, Date.now(), timeout / 1000);
-    actionSet.add(SetType.USER, user, Date.now(), timeout / 1000);
-    actionSet.add(SetType.COUNTRY, country, Date.now(), timeout / 1000);
-
-    setTimeout(function () {
-      expect(actionSet.has(SetType.IP, ip)).to.be.false;
-      expect(actionSet.has(SetType.USER, user)).to.be.false;
-      expect(actionSet.has(SetType.COUNTRY, country)).to.be.false;
+    setTimeout(() => {
+      expect(this.actionSet.has(SetType.IP, ip)).to.be.false;
+      expect(this.actionSet.has(SetType.USER, user)).to.be.false;
+      expect(this.actionSet.has(SetType.COUNTRY, country)).to.be.false;
       done();
     }, timeout);
-  });
+  }).timeout(2100);
 
-  it('Should expire only first ip', function (done) {
+  it('Should expire only first ip', (done) => {
     const ip = "10.0.0.1";
     const ip2 = "10.0.0.2";
     const timeout = 2000;
 
-    // set test timeout
-    this.timeout(timeout + 100);
+    this.actionSet.add(SetType.IP, ip, Date.now(), 1);
+    this.actionSet.add(SetType.IP, ip2, Date.now(), 10);
 
-    const actionSet = new ActionSet("ActionTest");
-    actionSet.add(SetType.IP, ip, Date.now(), 1);
-    actionSet.add(SetType.IP, ip2, Date.now(), 100);
-
-    setTimeout(function () {
-      expect(actionSet.has(SetType.IP, ip)).to.be.false;
-      expect(actionSet.has(SetType.IP, ip2)).to.be.true;
+    setTimeout(() => {
+      expect(this.actionSet.has(SetType.IP, ip)).to.be.false;
+      expect(this.actionSet.has(SetType.IP, ip2)).to.be.true;
       done();
     }, timeout);
-  });
+  }).timeout(2100);;
 
-  it('Should expire only first user', function (done) {
+  it('Should expire only first user', (done) => {
     const user = "12345";
     const user2 = "333333";
     const timeout = 2000;
 
-    // set test timeout
-    this.timeout(timeout + 100);
+    this.actionSet.add(SetType.USER, user, Date.now(), 1);
+    this.actionSet.add(SetType.USER, user2, Date.now(), 10);
 
-    const actionSet = new ActionSet("ActionTest");
-    actionSet.add(SetType.USER, user, Date.now(), 1);
-    actionSet.add(SetType.USER, user2, Date.now(), 10);
-
-    setTimeout(function () {
-      expect(actionSet.has(SetType.USER, user)).to.be.false;
-      expect(actionSet.has(SetType.USER, user2)).to.be.true;
+    setTimeout(() => {
+      expect(this.actionSet.has(SetType.USER, user)).to.be.false;
+      expect(this.actionSet.has(SetType.USER, user2)).to.be.true;
       done();
     }, timeout);
-  });
+  }).timeout(2100);
 
   it('Should block ip range forever', () => {
     const ipRange = "2.3.4.5/32";
     const validIP = "2.3.4.5";
     const invalidIP = "1.2.3.4";
+    this.actionSet.add(SetType.IP, ipRange);
 
-    const actionSet = new ActionSet("ActionTest");
-    actionSet.add(SetType.IP, ipRange);
-
-    expect(actionSet.has(SetType.IP, validIP)).to.be.true;
-    expect(actionSet.has(SetType.IP, invalidIP)).to.be.false;
+    expect(this.actionSet.has(SetType.IP, validIP)).to.be.true;
+    expect(this.actionSet.has(SetType.IP, invalidIP)).to.be.false;
   });
 
   it('Should ignore invalid ip', () => {
     const badIP = "2.3.45";
+    this.actionSet.add(SetType.IP, badIP);
 
-    const actionSet = new ActionSet("ActionTest");
-    actionSet.add(SetType.IP, badIP);
-
-    expect(actionSet.has(SetType.IP, badIP)).to.be.false;
+    expect(this.actionSet.has(SetType.IP, badIP)).to.be.false;
   });
 
   it('Should delete not existing ip, user, country', () => {
@@ -130,13 +120,12 @@ describe('SecureNative', () => {
     const user = "DC48C86C04DF0005FB4DE3629AB1F";
     const country = "US";
 
-    const actionSet = new ActionSet("ActionTest");
-    actionSet.delete(SetType.IP, ip);
-    actionSet.delete(SetType.USER, user);
-    actionSet.delete(SetType.COUNTRY, country);
+    this.actionSet.delete(SetType.IP, ip);
+    this.actionSet.delete(SetType.USER, user);
+    this.actionSet.delete(SetType.COUNTRY, country);
 
-    expect(actionSet.has(SetType.IP, ip)).to.be.false;
-    expect(actionSet.has(SetType.USER, user)).to.be.false;
-    expect(actionSet.has(SetType.COUNTRY, country)).to.be.false;
+    expect(this.actionSet.has(SetType.IP, ip)).to.be.false;
+    expect(this.actionSet.has(SetType.USER, user)).to.be.false;
+    expect(this.actionSet.has(SetType.COUNTRY, country)).to.be.false;
   });
 });
--- src/action-set.ts
+++ src/action-set.ts
@@ -2,61 +2,74 @@ import { IPSet } from 'futoin-ipset';
 import SetType from './enums/set-type';
 
 export class ActionSet {
-  private multiSet = {
-    ip: new IPSet(),
-    path: new Map<string, NodeJS.Timeout>(),
-    user: new Map<string, NodeJS.Timeout>(),
-    country: new Map<string, NodeJS.Timeout>()
-  };
+  private multiSet: Object;
 
-  constructor(private name: string) { }
+  constructor(private name: string) {
+    this.clear();
+  }
 
   public add(setType: SetType, item: string, ts?: number, ttl?: number) {
-    const intervalId = (ts && ttl !== -1) ? setTimeout(() => this.delete(setType, item), ttl * 1000 - (new Date().getTime() - ts)) : -1;
+    const expireTs = (ts && ttl !== -1) ? ts + ttl * 1000 : -1;
     const set = this.getSet(setType);
 
     if (setType === SetType.IP) {
       if (!this.isValidIP(set, item)) {
         return;
       }
-      set.add(item, { intervalId: intervalId });
+      set.add(item, { expireTs: expireTs });
     } else {
-      set.set(item, intervalId);
+      set.set(item, expireTs);
     }
+
+    this.removeExpiredSets();
   }
 
   public has(setType: SetType, item: string): boolean {
+    const now = Date.now();
     const set = this.getSet(setType);
-
+    let expireTs = null;
     if (setType === SetType.IP) {
       if (!this.isValidIP(set, item)) {
         return false;
       }
-      return set.match(item) !== undefined;
+      const ipRecord = set.match(item) || {};
+      expireTs = ipRecord.expireTs;
+    } else {
+      expireTs = set.get(item);
     }
-    return set.has(item);
+
+    if (expireTs) {
+      if (expireTs !== -1 && now > expireTs) {
+        // clear item
+        this.delete(setType, item);
+        return false;
+      }
+      return true;
+    }
+
+    return false;
   }
 
   public delete(setType: SetType, item: string) {
-    let intervalId = null;
+    let expireTs = null;
     const set = this.getSet(setType);
     if (setType === SetType.IP) {
       if (!this.isValidIP(set, item)) {
         return;
       }
-      const match = set.match(item);
-      if (match) {
-        intervalId = match.intervalId;
-      }
       set.remove(item);
     } else {
-      intervalId = set.get(item);
       set.delete(item);
     }
+  }
 
-    if (intervalId) {
-      clearInterval(intervalId);
-    }
+  public clear() {
+    this.multiSet = {
+      ip: new IPSet(),
+      path: new Map<string, Number>(),
+      user: new Map<string, Number>(),
+      country: new Map<string, Number>()
+    };
   }
 
   private getSet(setType: SetType) {
@@ -68,6 +81,34 @@ export class ActionSet {
       return ipset.convertAddress(ip) !== null;
     } catch (ex) {
       return false;
+    }
+  }
+
+  private removeExpiredSets() {
+    for (let setType in SetType) {
+      this.removeExpiredSet(SetType[setType]);
+    }
+  }
+
+  private removeExpiredSet(setType: SetType) {
+    const set = this.getSet(setType);
+
+    if (setType === SetType.IP) {
+      for (let [sub, m] of set._v4._pm) {
+        for (let key of m.keys()) {
+          this.has(setType, key + "/" + sub);
+        }
+      }
+
+      for (let [sub, m] of set._v6._pm) {
+        for (let key of m.keys()) {
+          set.has(key + "/" + sub);
+        }
+      }
+    } else {
+      for (let key of set) {
+        this.has(setType, key);
+      }
     }
   }
 }
